f8ed27429442b3fcc3b0a58135581f559a34a3c0
karthik technologies <karthikyandra11@gmail.com>
2026-06-04 13:22:29 +0530

    post.py

--- post.py
+++ post.py
@@ -1,11 +1,13 @@
 from google import genai
 import os
 
-client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+client = genai.Client(
+    api_key=os.environ["GEMINI_API_KEY"]
+)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Generate an Instagram caption and 15 hashtags about AI tools."
+    contents="Generate an Instagram caption about AI tools."
 )
 
 print(response.text)
